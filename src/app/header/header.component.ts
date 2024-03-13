175b215d017a64e360d6fc032a5f92ea416e65df
PradipKhandare <pradipkhandare111@gmail.com>
2024-03-13 18:14:42 +0530

    make side bar UI using angular, html, scss

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  @Input() sidebarActive = false
+
+  @Output() toggleSidebar = new EventEmitter<any>();
+
+
+  constructor() { }
+}
